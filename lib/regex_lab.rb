b1926e70eb3f5071c9d0f8e29a7097a6028a8364
Eric McKinney <mckinney99@gmail.com>
2017-10-31 13:21:38 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,13 @@
 def starts_with_a_vowel?(word)
-
+  if word.match (/\A[aeiouAEIOU]/)
+    return true
+  else
+    return false
+end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/un\w+ing/)
 end
 
 def words_five_letters_long(text)
